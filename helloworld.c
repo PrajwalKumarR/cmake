7328fbe04506459ee598a47d756643dc73e43caa
PrajwalKumarR <69086572+PrajwalKumarR@users.noreply.github.com>
2022-01-13 17:56:11 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,6 @@
 void helloworld(){
 //int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World! , poll SCM triggered and changes updated");
  //  return 0;
 }
